f2dee363c01bd6fd0353aa79cbbaf0fb8c7ed868
xiongli006 <Xl931123>
2019-03-30 00:17:30 +0800

    2333

--- public/js/donghua.js
+++ public/js/donghua.js
@@ -2,82 +2,97 @@
 // JavaScript Document
 
 
-
 //产品
-$(window).scroll(function() {	
-			if ($(window).scrollTop() > 1000) {
-				$(".cpright ul li:nth-child(1)").attr('id','atc1');
-				$(".cpright ul li:nth-child(2)").attr('id','atc2');
-				$(".cpright ul li:nth-child(3)").attr('id','atc3');
-				$(".cpright ul li:nth-child(4)").attr('id','atc4');
-				$(".cpright ul li:nth-child(5)").attr('id','atc5');
-				$(".cpright ul li:nth-child(6)").attr('id','atc6');
-				$(".cpright ul li:nth-child(7)").attr('id','atc7');
-				$(".cpright ul li:nth-child(8)").attr('id','atc8');
-			} else {
-				$(".cpright ul li:nth-child(1)").removeAttr('id','atc1');
-				$(".cpright ul li:nth-child(2)").removeAttr('id','atc2');
-				$(".cpright ul li:nth-child(3)").removeAttr('id','atc3');
-				$(".cpright ul li:nth-child(4)").removeAttr('id','atc4');
-				$(".cpright ul li:nth-child(5)").removeAttr('id','atc5');
-				$(".cpright ul li:nth-child(6)").removeAttr('id','atc6');
-				$(".cpright ul li:nth-child(7)").removeAttr('id','atc7');
-				$(".cpright ul li:nth-child(8)").removeAttr('id','atc8');
-				
-			}											
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 1000) {
+    $(".cpright ul li:nth-child(1)").attr('id', 'atc1');
+    $(".cpright ul li:nth-child(2)").attr('id', 'atc2');
+    $(".cpright ul li:nth-child(3)").attr('id', 'atc3');
+    $(".cpright ul li:nth-child(4)").attr('id', 'atc4');
+    $(".cpright ul li:nth-child(5)").attr('id', 'atc5');
+    $(".cpright ul li:nth-child(6)").attr('id', 'atc6');
+    $(".cpright ul li:nth-child(7)").attr('id', 'atc7');
+    $(".cpright ul li:nth-child(8)").attr('id', 'atc8');
+  } else {
+    $(".cpright ul li:nth-child(1)").removeAttr('id', 'atc1');
+    $(".cpright ul li:nth-child(2)").removeAttr('id', 'atc2');
+    $(".cpright ul li:nth-child(3)").removeAttr('id', 'atc3');
+    $(".cpright ul li:nth-child(4)").removeAttr('id', 'atc4');
+    $(".cpright ul li:nth-child(5)").removeAttr('id', 'atc5');
+    $(".cpright ul li:nth-child(6)").removeAttr('id', 'atc6');
+    $(".cpright ul li:nth-child(7)").removeAttr('id', 'atc7');
+    $(".cpright ul li:nth-child(8)").removeAttr('id', 'atc8');
+
+  }
 });
 
 //新闻
-$(window).scroll(function() {	
-			if ($(window).scrollTop() > 1500) {
-				$(".newright ul li:nth-child(1)").animate({opacity:'1'},500);
-				$(".newright ul li:nth-child(2)").animate({opacity:'1'},1000);
-				$(".newright ul li:nth-child(3)").animate({opacity:'1'},1500);
-				$(".newright ul li:nth-child(4)").animate({opacity:'1'},2000);
-			} else {
-			}											
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 1500) {
+    $(".newright ul li:nth-child(1)").animate({ opacity: '1' }, 500);
+    $(".newright ul li:nth-child(2)").animate({ opacity: '1' }, 1000);
+    $(".newright ul li:nth-child(3)").animate({ opacity: '1' }, 1500);
+    $(".newright ul li:nth-child(4)").animate({ opacity: '1' }, 2000);
+  } else {
+  }
 });
 //新闻
-$(window).scroll(function() {	
-			if ($(window).scrollTop() > 1500) {
-				$(".dianshi").attr('id','tc1');
-				
-			} else {
-				$(".dianshi").removeAttr('id','tc1');
-			}											
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 1500) {
+    $(".dianshi").attr('id', 'tc1');
+
+  } else {
+    $(".dianshi").removeAttr('id', 'tc1');
+  }
 });
-		
+
 //案例
-$(window).scroll(function() {	
-			if ($(window).scrollTop() > 2000) {
-				$(".anliMain ul li:nth-child(1)").attr('id','tc1');
-				$(".anliMain ul li:nth-child(2)").attr('id','tc2');
-				$(".anliMain ul li:nth-child(3)").attr('id','tc3');
-				$(".anliMain ul li:nth-child(4)").attr('id','tc4');
-				
-			} else {
-				$(".anliMain ul li:nth-child(1)").removeAttr('id','tc1');
-				$(".anliMain ul li:nth-child(2)").removeAttr('id','tc2');
-				$(".anliMain ul li:nth-child(3)").removeAttr('id','tc3');
-				$(".anliMain ul li:nth-child(4)").removeAttr('id','tc4');
-			}											
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 2000) {
+    $(".anliMain ul li:nth-child(1)").attr('id', 'tc1');
+    $(".anliMain ul li:nth-child(2)").attr('id', 'tc2');
+    $(".anliMain ul li:nth-child(3)").attr('id', 'tc3');
+    $(".anliMain ul li:nth-child(4)").attr('id', 'tc4');
+    $(".anliMain ul li:nth-child(5)").attr('id', 'tc5');
+    $(".anliMain ul li:nth-child(6)").attr('id', 'tc5');
+
+  } else {
+    $(".anliMain ul li:nth-child(1)").removeAttr('id', 'tc1');
+    $(".anliMain ul li:nth-child(2)").removeAttr('id', 'tc2');
+    $(".anliMain ul li:nth-child(3)").removeAttr('id', 'tc3');
+    $(".anliMain ul li:nth-child(4)").removeAttr('id', 'tc4');
+    $(".anliMain ul li:nth-child(5)").removeAttr('id', 'tc5');
+    $(".anliMain ul li:nth-child(6)").removeAttr('id', 'tc6');
+  }
 });
 
 //合作伙伴
-$(window).scroll(function() {	
-			if ($(window).scrollTop() > 2500) {
-				$(".hezuoMain ul li:nth-child(1)").attr('id','tc1');
-				$(".hezuoMain ul li:nth-child(2)").attr('id','tc2');
-				$(".hezuoMain ul li:nth-child(3)").attr('id','tc3');
-				$(".hezuoMain ul li:nth-child(4)").attr('id','tc4');	
-				$(".hezuoMain ul li:nth-child(5)").attr('id','tc5');
-			} else {
-				$(".hezuoMain ul li:nth-child(1)").removeAttr('id','tc1');
-				$(".hezuoMain ul li:nth-child(2)").removeAttr('id','tc2');
-				$(".hezuoMain ul li:nth-child(3)").removeAttr('id','tc3');
-				$(".hezuoMain ul li:nth-child(4)").removeAttr('id','tc4');
-				$(".hezuoMain ul li:nth-child(5)").removeAttr('id','tc5');
-			}											
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 2500) {
+    $(".hezuoMain ul li:nth-child(1)").attr('id', 'tc1');
+    $(".hezuoMain ul li:nth-child(2)").attr('id', 'tc2');
+    $(".hezuoMain ul li:nth-child(3)").attr('id', 'tc3');
+    $(".hezuoMain ul li:nth-child(4)").attr('id', 'tc4');
+    $(".hezuoMain ul li:nth-child(5)").attr('id', 'tc5');
+    $(".hezuoMain ul li:nth-child(6)").attr('id', 'tc6');
+    $(".hezuoMain ul li:nth-child(7)").attr('id', 'tc7');
+    $(".hezuoMain ul li:nth-child(8)").attr('id', 'tc8');
+    $(".hezuoMain ul li:nth-child(9)").attr('id', 'tc9');
+    $(".hezuoMain ul li:nth-child(10)").attr('id', 'tc10');
+    $(".hezuoMain ul li:nth-child(11)").attr('id', 'tc11');
+  } else {
+    $(".hezuoMain ul li:nth-child(1)").removeAttr('id', 'tc1');
+    $(".hezuoMain ul li:nth-child(2)").removeAttr('id', 'tc2');
+    $(".hezuoMain ul li:nth-child(3)").removeAttr('id', 'tc3');
+    $(".hezuoMain ul li:nth-child(4)").removeAttr('id', 'tc4');
+    $(".hezuoMain ul li:nth-child(5)").removeAttr('id', 'tc5');
+    $(".hezuoMain ul li:nth-child(6)").removeAttr('id', 'tc6');
+    $(".hezuoMain ul li:nth-child(7)").removeAttr('id', 'tc7');
+    $(".hezuoMain ul li:nth-child(8)").removeAttr('id', 'tc8');
+    $(".hezuoMain ul li:nth-child(9)").removeAttr('id', 'tc9');
+    $(".hezuoMain ul li:nth-child(10)").removeAttr('id', 'tc10');
+    $(".hezuoMain ul li:nth-child(11)").removeAttr('id', 'tc11');
+  }
 });
 
 $(function () {
